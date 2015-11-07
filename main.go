be268bbae49f28ad8a74b3429453c30e6d3bb44e
Mickael FORTUNATO <morsi.morsicus@gmail.com>
2015-11-07 22:58:58 +0100

    Some things for JSON format.

--- main.go
+++ main.go
@@ -2,24 +2,46 @@ package main
 
 import (
 	"fmt"
-	"os"
+	//"os"
 	"log"
 	"net/http"
+	"encoding/json"
+	//"github.com/fzzy/radix/redis"
 	"github.com/gorilla/mux"
+
 )
 
 type redisEntry struct {
-	Key    string
-	Value  string
+	Key    string `json:"key"`
+	Value  string `json:"value"`
 }
 
 func Index(w http.ResponseWriter, r *http.Request) {
+	data := &redisEntry{
+		Key: "Yolo",
+		Value: "PORRROOO",
+	}
+	myJson, _ := json.Marshal(data)
+	fmt.Println(string(myJson))
 }
 
 func CmdKeys(w http.ResponseWriter, r *http.Request) {
+	data := &redisEntry{
+		Key: "Yolo",
+		Value: "PORRROOO",
+	}
+	myJson, _ := json.Marshal(data)
+	fmt.Println(string(myJson))
 }
 
 func CmdGetKey(w http.ResponseWriter, r *http.Request) {
+	data := &redisEntry{
+		Key: "Yolo",
+		Value: "PORRROOO",
+	}
+	myJson, _ := json.Marshal(data)
+	fmt.Println(string(myJson))
+
 }
 
 func main() {
