6ed14409002d7ddcf95bd37ba297b818ac65b247
Mickael FORTUNATO <morsi.morsicus@gmail.com>
2015-11-07 23:56:43 +0100

    Little experiment about connection to redis database. BE CAREFUL HARDCODED IP OF REDIS DATABASE

--- main.go
+++ main.go
@@ -2,18 +2,25 @@ package main
 
 import (
 	"fmt"
-	//"os"
+	"os"
 	"log"
+	"time"
 	"net/http"
 	"encoding/json"
-	//"github.com/fzzy/radix/redis"
+	"github.com/fzzy/radix/redis"
 	"github.com/gorilla/mux"
-
 )
 
 type redisEntry struct {
 	Key    string `json:"key"`
 	Value  string `json:"value"`
+}
+
+func errHandler(err error) {
+	if err != nil {
+		fmt.Println("error:", err)
+		os.Exit(1)
+	}
 }
 
 func Index(w http.ResponseWriter, r *http.Request) {
@@ -35,13 +42,17 @@ func CmdKeys(w http.ResponseWriter, r *http.Request) {
 }
 
 func CmdGetKey(w http.ResponseWriter, r *http.Request) {
-	data := &redisEntry{
-		Key: "Yolo",
-		Value: "PORRROOO",
-	}
-	myJson, _ := json.Marshal(data)
-	fmt.Println(string(myJson))
+	vars := mux.Vars(r)
+	key := vars["key"]
 
+	c, err := redis.DialTimeout("tcp", "172.17.0.2:6379", time.Duration(10)*time.Second)
+	errHandler(err)
+	defer c.Close()
+
+	res, err := c.Cmd("get", key).Str()
+	errHandler(err)
+
+	fmt.Println(res)
 }
 
 func main() {
